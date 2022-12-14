9ee303b5f7befe87e264fff3efac1da657700dca
Dany Cervantes <danycervantesq@gmail.com>
2022-12-14 03:13:22 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -115,11 +115,11 @@ function Circle(x,y,dx,dy,radius,color) {
 
             //sets left & right boundaries
             if(this.x + this.radius + this.dx >= screenWidth || this.x + this.dx <= this.radius) {
-                this.dx = -this.dx * this.frictionX; //reduces side movement on side bounce
+                this.dx = -this.dx * (this.frictionX - mass()); //reduces side movement on side bounce
             }
             //sets ceiling & floor boundaries
             if(this.y + this.radius + this.dy >= screenHeight || this.y + this.dy <= this.radius) {
-                this.dy = -this.dy * this.frictionY;  //reduces upward movement on floor bounce
+                this.dy = -this.dy * (this.frictionY - mass());  //reduces upward movement on floor bounce
             } else {
                 this.dy += this.gravity + mass(); //gravity
             }
